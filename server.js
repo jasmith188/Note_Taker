bf2e00da1dba94d58e975fce31ecb6bf287b2f89
Jimmy Smith <jasmith.188@gmail.com>
2020-04-08 19:14:20 -0400

    setting up page

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Dependencies
+// =============================================================
 const express = require("express");
-
+// Sets up the Express App
+// =============================================================
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
+// Sets up the Express app to handle data parsing
 app.use(Express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -11,9 +13,19 @@ app.use(express.static("public"));
 
 
 
+// Routes
+// =============================================================
 
 
+//API Get route
 app.listen(PORT, () => {
     console.log(`Listenong on port: ${PORT}`)
     
 })
+
+//API POST Route
+
+
+
+
+//API Delete ROute
