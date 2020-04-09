d8f7867e9a894d95462039387bda629e4d2f4708
Jimmy Smith <jasmith.188@gmail.com>
2020-04-09 17:06:04 -0400

    working

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 // Dependencies
 // =============================================================
 const express = require("express");
+const path = require("path");
+const fs = require("fs");
 // Sets up the Express App
 // =============================================================
 const app = express();
+//deployable port and local port
 const PORT = process.env.PORT || 3000;
-// Sets up the Express app to handle data parsing
-app.use(Express.urlencoded({ extended: true}));
+// loads all the middleware
+app.use(Express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 
+//HTML routes - notes
+
+app.post("/api/notes", (req,res) => {
+
+    const remote = req.body;
+    //add id property to object
+    newNote.id = //create Id////
+    
+})
 
 
 // Routes
@@ -19,8 +31,8 @@ app.use(express.static("public"));
 
 //API Get route
 app.listen(PORT, () => {
-    console.log(`Listenong on port: ${PORT}`)
-    
+    console.log(`Listening on port: ${PORT}`)
+
 })
 
 //API POST Route
@@ -28,4 +40,9 @@ app.listen(PORT, () => {
 
 
 
-//API Delete ROute
+//API Delete Route
+
+
+
+
+//HTML routes - get all ***
