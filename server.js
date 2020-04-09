15bc07be9a9f2003d913754ceedbd3c25d59ada1
Jimmy Smith <jasmith.188@gmail.com>
2020-04-09 19:05:00 -0400

    fixe errors

--- server.js
+++ server.js
@@ -9,29 +9,29 @@ const app = express();
 //deployable port and local port
 const PORT = process.env.PORT || 3000;
 // loads all the middleware
-app.use(Express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 // Routes ============================================================= //
 
 //HTML get routes - notes
-app.get("/notes",  (req, res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 //API Get Route notes
-app.get("/api/notes", (req, res) => {
-    fs.writeFile('db/db.json', function (req, res) {
+app.get("/api/notes", function (req, res) {
+    fs.readFile('db/db.json', function (req, res) {
         if (err) throw err;
         return res.json(JSON.parse(data));
-    })
-    
+    });
 
+})
 //API POST Route
 // Create New Characters - takes in JSON input
 app.post("/api/notes", function (req, res) {
-// req.body hosts is equal to the JSON post sent from the user
-// This works because of our body parsing middleware
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
     var newNotes = req.body;
     fs.readFile("db/db.json", function (err, data) {
         if (err) throw err;
@@ -39,14 +39,14 @@ app.post("/api/notes", function (req, res) {
         req.body.id = notes.length + 1;
         notes.push(newNotes);
 
-    fs.writeFile('db/db.json', JSON.stringify(notes), function (err) {
-        if (err) throw err;
-        return res.json(newCharacter);
-    })
-    
-});
+        fs.writeFile('db/db.json', JSON.stringify(notes), function (err) {
+            if (err) throw err;
+            return res.json(newNotes);
+        })
 
+    });
 
+})
 
 //API Delete Route
 // ---------------------------------------------------------------------------
@@ -55,10 +55,10 @@ app.post("/api/notes", function (req, res) {
 
 app.delete("/api/notes/:id", function (req, res) {
     // Empty out the arrays of data
-    notes = notes.filter(note=>note.id != req.params.id)
+    notes = notes.filter(note => note.id != req.params.id)
     fs.writeFile('db/db.json', JSON.stringify(notes), function (err) {
         if (err) throw err;
-        return res.json(newCharacter);
+        return res.json(newNotes);
     })
 
     res.json({ ok: true });
@@ -68,7 +68,9 @@ app.delete("/api/notes/:id", function (req, res) {
 
 
 //HTML routes - get all * will return to index.html
-
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
